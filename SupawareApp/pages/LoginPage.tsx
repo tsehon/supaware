0330844e501644e8a24b7c3a5226758182241b6b
Tyler Sehon <tsehon@usc.edu>
2023-04-09 16:55:49 -0700

    login page styling

--- SupawareApp/pages/LoginPage.tsx
+++ SupawareApp/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { API_URL } from 'react-native-dotenv';
 import { AuthContext } from '../App';
@@ -22,18 +22,22 @@ const LoginPage = ({ navigation }) => {
     };
 
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
             <TextInput
+                style={styles.input}
                 value={username}
                 onChangeText={setUsername}
                 placeholder="Username"
+                autoCapitalize="none"
             />
             <TextInput
+                style={styles.input}
                 value={password}
                 onChangeText={setPassword}
                 placeholder="Password"
                 secureTextEntry
+                autoCapitalize="none"
             />
             <Button title="Login" onPress={loginUser} />
             <Button
@@ -43,5 +47,25 @@ const LoginPage = ({ navigation }) => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        marginBottom: 15,
+    },
+});
 
 export default LoginPage;
