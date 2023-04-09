01dab16a94abdf0b559eed3d59406fc2d43233ef
Tyler Sehon <tsehon@usc.edu>
2023-04-09 16:54:02 -0700

    registerpage styling

--- SupawareApp/pages/RegisterPage.tsx
+++ SupawareApp/pages/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { API_URL } from 'react-native-dotenv';
 
@@ -24,22 +24,46 @@ const RegisterPage: React.FC<RegisterPageProps> = ({ onRegisterSuccess }) => {
     };
 
     return (
-        <View>
-            <Text>Register</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Register</Text>
             <TextInput
+                style={styles.input}
                 value={username}
                 onChangeText={setUsername}
                 placeholder="Username"
+                autoCapitalize="none"
             />
             <TextInput
+                style={styles.input}
                 value={password}
                 onChangeText={setPassword}
                 placeholder="Password"
                 secureTextEntry
+                autoCapitalize="none"
             />
             <Button title="Register" onPress={registerUser} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        marginBottom: 15,
+    },
+});
 
 export default RegisterPage;
