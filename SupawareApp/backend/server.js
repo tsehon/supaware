02b4d88085ddcfb39cfc493d16d4b46cb348ebf1
Tyler Sehon <tsehon@usc.edu>
2023-04-15 16:44:56 -0700

    mongodb deployment connected

--- SupawareApp/backend/server.js
+++ SupawareApp/backend/server.js
@@ -13,69 +13,83 @@ app.use(express.json());
 // Connect to MongoDB
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 
-client.connect((err) => {
-    if (err) {
-        console.error('Failed to connect to MongoDB:', err);
-        return;
+console.log('Connecting to MongoDB...');
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+// Access the "users" collection in your database
+const usersCollection = client.db('supaware-db').collection('users');
+
+// Register route
+app.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
+
+    const existingUser = await usersCollection.findOne({ username });
+    if (existingUser) {
+        return res.status(400).json({ message: 'Username already exists' });
     }
 
-    // Access the "users" collection in your database
-    const usersCollection = client.db('your_database_name').collection('users');
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = { username, password: hashedPassword };
+        await usersCollection.insertOne(newUser);
 
-    // Register route
-    app.post('/register', async (req, res, next) => {
-        const { username, password } = req.body;
-
-        const existingUser = users.find((u) => u.username === username);
-        if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-
-        try {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const newUser = { username, password: hashedPassword };
-            await usersCollection.insertOne(newUser);
-
-            res.status(201).json({ message: 'User registered successfully' });
-        } catch (err) {
-            next(err);
-        }
-    });
-
-    // Login route
-    app.post('/login', async (req, res) => {
-        const { username, password } = req.body;
-
-        const user = await usersCollection.findOne({ username });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) {
-            return res.status(401).json({ message: 'Invalid password' });
-        }
-
-        const token = jwt.sign({ userId: user.username }, process.env.JWT_SECRET);
-        res.json({ token });
-    });
-
-    // Error handling middleware
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).json({ message: 'An error occurred' });
-    });
-
-    const PORT = process.env.PORT || 3001;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-
-    // Close the connection when the server is stopped
-    process.on('SIGINT', () => {
-        client.close();
-        process.exit();
-    });
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        next(err);
+    }
 });
+
+// Login route
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await usersCollection.findOne({ username });
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ userId: user.username }, process.env.JWT_SECRET);
+    res.json({ token });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'An error occurred' });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Close the connection when the server is stopped
+process.on('SIGINT', () => {
+    client.close();
+    process.exit();
+});
+
+run().catch(console.dir);
